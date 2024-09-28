1e2c08bd94f14cb001eb3e972a0da5efbc1fa95f
Olivia Waithira <oliviawaithira7@gmail.com>
2024-09-28 23:23:23 +0300

    javascript

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const decreaseBtn = document.getElementById("decreaseBtn");
 const resetBtn = document.getElementById("resetBtn");
 const increaseBtn = document.getElementById("increaseBtn");
+const countLbel = document.getElementById("countLabel");
+let count = 0;
+
+increaseBtn.onclick = function(){
+    count++;
+    countLabel.textContent = count;
+}
